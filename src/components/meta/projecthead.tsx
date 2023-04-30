37887059173e61cf85e3fb05f386d7a8b04ada06
LEE-YO-HAN <yhl0078@gmail.com>
2023-04-30 16:53:34 +0900

    Update: meta tags for naver/google search engine

---
+++ src/components/meta/projecthead.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export const ProjectHead = () => {
+  return (
+    <Head>
+      <meta
+        name="naver-site-verification"
+        content="e0d247e78eb506cbac8e977f8873ccb6e7f9ca62"
+      />
+      <meta
+        name="google-site-verification"
+        content="uTP-ib81rGQtYFUvRehwY0dpJZc1DWsoIgmELpRoNws"
+      />
+    </Head>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,14 @@ import { light } from "@/styles/theme";
 import { Layout } from "@/components/layout/layout";
 import { EclipsLoadingSpinner } from "@/util/eclipsLoadingSpinner";
 import { usePageLoading } from "@/hooks/usePageLoading";
-
+import { ProjectHead } from "@/components/meta/projecthead";
 export default function App({ Component, pageProps }: AppProps) {
   const isLoading = usePageLoading();
 
   return (
     <Provider store={store}>
       <ThemeProvider theme={light}>
+        <ProjectHead />
         <Layout>
           <Component {...pageProps} />
         </Layout>
